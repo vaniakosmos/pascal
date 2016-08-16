2c899be830c6df2e3c4aed2e2a53b1eaab5f4157
Bachynin Ivan <bachynin.i@gmail.com>
2016-08-16 22:24:47 +0300

    hardcode reverse polish notation
    works fine but looks ugly

--- calc/interpreter.py
+++ calc/interpreter.py
@@ -22,27 +22,69 @@ class Interpreter(object):
         self.eat(INTEGER)
         return token.value
 
+    @staticmethod
+    def read_rpn(notation):
+        stack = []
+        for token in notation:
+            if token not in OPERATIONS:
+                stack.append(token)
+            else:
+                y = stack.pop(-1)
+                x = stack.pop(-1)
+                if token == PLUS:
+                    stack.append(x + y)
+
+                elif token == MINUS:
+                    stack.append(x - y)
+
+                elif token == MUL:
+                    stack.append(x * y)
+
+                elif token == DIV:
+                    stack.append(x // y)
+
+                else:
+                    Interpreter.error()
+                # print('{} {} {} = {}'.format(x, token, y, stack[-1]))
+        return stack[0]
+
     def expr(self):
         self.current_token = self.lexer.get_next_token()
 
-        result = self.term()
+        notation = []
+        ops = []
 
-        while self.current_token.type in (PLUS, MINUS):
+        last_high = False
+        while self.current_token.type != EOF:
             token = self.current_token
-            if token.type == PLUS:
-                self.eat(PLUS)
-                result = result + self.term()
-            elif token.type == MINUS:
-                self.eat(MINUS)
-                result = result - self.term()
 
-        while self.current_token.type in (MUL, DIV):
-            token = self.current_token
-            if token.type == MUL:
-                self.eat(MUL)
-                result = result * self.term()
-            elif token.type == DIV:
-                self.eat(DIV)
-                result = result / self.term()
+            if token.type == INTEGER:
+                if last_high:
+                    notation[-1][-2] = token.value
+                else:
+                    notation.append([token.value])
 
-        return result
+            elif token.type in (MUL, DIV):
+                last_high = True
+                notation[-1].append(None)
+                notation[-1].append(token.type)
+
+            elif token.type in (PLUS, MINUS):
+                last_high = False
+                ops.append(token.type)
+
+            else:
+                self.error()
+
+            self.current_token = self.lexer.get_next_token()
+
+        res = notation[0]
+        i = 0
+        for note in notation[1:]:
+            res += note + [ops[i]]
+            i += 1
+
+        return self.read_rpn(res)
+
+if __name__ == '__main__':
+    print(Interpreter.read_rpn([14, 2, 3, 'MUL', 'PLUS', 6, 2, 'DIV', 'MINUS']))
