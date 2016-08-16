db3226f019a66aeb604f4aec308caa7e00140b2f
Bachynin Ivan <bachynin.i@gmail.com>
2016-08-16 05:40:30 +0300

    add *, / and multiple values calculation (valid only for + and -)

--- calculator.py
+++ calculator.py
@@ -1,6 +1,5 @@
-from re import sub
-
-INTEGER, PLUS, MINUS, EOF = 'INTEGER', 'PLUS', 'MINUS', 'EOF'
+INTEGER, EOF = 'INTEGER', 'EOF'
+PLUS, MINUS, MULTI, DIV = 'PLUS', 'MINUS', 'MULTI', 'DIV'
 
 
 class Token(object):
@@ -20,72 +19,106 @@ class Token(object):
 
 class Interpreter(object):
     def __init__(self, text):
-        self.text = sub(' ', '', text)
+        self.text = text
         self.pos = 0
         self.current_token = None
+        self.current_char = self.text[self.pos]
 
     @staticmethod
     def error():
         raise Exception('Error parsing input')
 
+    def advance(self):
+        """Advance the 'pos' pointer and set the 'current_char' variable."""
+        self.pos += 1
+        if self.pos > len(self.text) - 1:
+            self.current_char = None  # Indicates end of input
+        else:
+            self.current_char = self.text[self.pos]
+
+    def skip_whitespace(self):
+        while self.current_char is not None and self.current_char.isspace():
+            self.advance()
+
+    def integer(self):
+        """Return a (multidigit) integer consumed from the input."""
+        result = ''
+        while self.current_char is not None and self.current_char.isdigit():
+            result += self.current_char
+            self.advance()
+        return int(result)
+
     def get_next_token(self):
         """Lexical analyzer
 
         This method is responsible for breaking a sentence
-        apart into tokens. One token at a time.
+        apart into tokens.
         """
-        text = self.text
+        while self.current_char is not None:
 
-        if self.pos > len(text) - 1:
-            return Token(EOF, None)
+            if self.current_char.isspace():
+                self.skip_whitespace()
+                continue
 
-        current_char = text[self.pos]
+            if self.current_char.isdigit():
+                return Token(INTEGER, self.integer())
 
-        if current_char.isdigit():
-            token = Token(INTEGER, int(current_char))
-            self.pos += 1
-            return token
+            if self.current_char == '+':
+                self.advance()
+                return Token(PLUS, '+')
 
-        if current_char == '+':
-            token = Token(PLUS, current_char)
-            self.pos += 1
-            return token
+            if self.current_char == '-':
+                self.advance()
+                return Token(MINUS, '-')
 
-        if current_char == '-':
-            token = Token(MINUS, current_char)
-            self.pos += 1
-            return token
+            if self.current_char == '*':
+                self.advance()
+                return Token(MULTI, '*')
 
-        self.error()
+            if self.current_char == '/':
+                self.advance()
+                return Token(DIV, '/')
 
-    def eat(self, valid_token_types):
-        if self.current_token.type in valid_token_types:
-            self.current_token = self.get_next_token()
-        else:
             self.error()
 
-    def get_big_int(self):
-        big_int = 0
-        while self.current_token.type == INTEGER:
-            left = self.current_token
-            self.eat([INTEGER])
-            big_int = big_int * 10 + left.value
-        return big_int
+        return Token(EOF, None)
+
+    def eat(self, token_type):
+        if self.current_token.type == token_type:
+            self.current_token = self.get_next_token()
+            print(self.current_token)
+        else:
+            self.error()
 
     def expr(self):
         self.current_token = self.get_next_token()
 
-        left = self.get_big_int()
+        result = self.current_token.value
+        self.eat(INTEGER)
 
-        op = self.current_token
-        self.eat([PLUS, MINUS])
+        while self.current_token.type is not EOF:
+            op = self.current_token
+            if op.type == PLUS:
+                self.eat(PLUS)
+            elif op.type == MINUS:
+                self.eat(MINUS)
+            elif op.type == MULTI:
+                self.eat(MULTI)
+            else:
+                self.eat(DIV)
 
-        right = self.get_big_int()
+            right = self.current_token
+            self.eat(INTEGER)
 
-        if op.type == PLUS:
-            result = left + right
-        else:
-            result = left - right
+            if op.type == PLUS:
+                result += right.value
+            elif op.type == MINUS:
+                result -= right.value
+            elif op.type == MULTI:
+                result *= right.value
+            else:
+                result /= right.value
+
         return result
 
 
